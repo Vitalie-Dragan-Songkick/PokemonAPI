33a7c4c2f31fcb4694ca1c59859178aa6a737bd4
Vitalie <138406664+Vitalie-Dragan@users.noreply.github.com>
2024-04-16 17:03:50 +0100

    commit all

---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react';
+import SearchBasicExample from './search.tsx';
+// import './app.css';
+
+
+interface Pokemon {
+  name: string;
+  url: string;
+  image: string;
+  sprites: {
+    'official-artwork': {
+      front_default: string;
+    };
+  };
+}
+
+const App: React.FC = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  
+  useEffect(() => {
+    async function fetchPokemons() {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=500');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        const pokemonDetailsPromises = data.results.map(async (pokemon: any) => {
+          const pokemonResponse = await fetch(pokemon.url);
+          if (!pokemonResponse.ok) {
+            throw new Error('Failed to fetch pokemon details');
+          }
+          const pokemonData = await pokemonResponse.json();
+            return {
+              name: pokemonData.name,
+              image: pokemonData.sprites.other['official-artwork'].front_default,
+              // image: pokemonData.sprites.versions['generation-v']['black-white']['animated'].front_default,
+              natural_gift_power: pokemonData.natural_gift_power,
+              type: pokemonData.types.map((type: any) => type.type.name).join(', '),
+              id: pokemonData.id,
+            };
+        });
+        const pokemonDetails = await Promise.all(pokemonDetailsPromises);
+        setPokemons(pokemonDetails);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    
+    fetchPokemons();
+  }, []);
+
+  const filteredPokemons = pokemons.filter(pokemon =>
+    pokemon.name.toUpperCase().includes(searchTerm.toUpperCase()),
+  );
+  // console.log(filteredPokemons.map(pokemon => pokemon.name));
+  // console.log(filteredPokemons.map(pokemon => pokemon.url));
+
+  return (
+    <>
+      <h1 className='flex items-center text-1xl sm:text-2xl bg-stone-700 w-full text-white p-2'> {/*SMALL SCREEN   size-4/12 */}
+        <img src='public/img/logo.png' alt='Logo' className='size-2/12 mr-2 sm:mr-4 sm:size-1/12' /> {/*SMALL SCREEN   size-4/12 */}
+        <span className='font-bold mr-1 sm:mr-2'>Pokeverse</span>  | All Pokemon
+      </h1>
+      <SearchBasicExample searchTerm={searchTerm} setSearchTerm={setSearchTerm} /> {/* DISPLAY CENTER */}
+      
+
+      <div className='grid grid-cols-1 sm:grid-cols-4'>
+        {filteredPokemons.map((pokemon, index) => (
+          <div className="grid items-center justify-items-center m-4 py-8 px-0 max-w-sm mx-auto bg-white rounded-xl shadow-xl space-y-6" key={index}>
+            <img src={pokemon.image} alt={pokemon.name} className="rounded-lg  w-4/5 h-4/5 pt-0 border-t-0 scale-150" />
+            <div className='justify-items-left text-4xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-pink-900 to-red-500'>{pokemon.name.toLocaleUpperCase()}</div>
+            {/* <div className='justify-items-left '>Level: {pokemon.natural_gift_power}</div> */}
+
+
+            <button className=" px-8 py-2 text-sm text-pink-600 font-bold rounded-lg border border-purple-200 hover:text-white  hover:bg-pink-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-pink-600 focus:ring-offset-2 ease-in-out duration-300">
+              Find Out More
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+    // Reaponse.all(data).then((results) => {
+    // const pokemon = results.map((result) => ({
+    //     name: result.name,
+    //     image: result.sprites['front_default'],
+    //     type: result.types.map((type) => type.type.name).join(', '),
+    //     id: result.id
+    // }));
+    // displayPokemon(pokemon);
+    // });
---
+++ src/search.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { TERipple } from 'tw-elements-react';
+
+interface SearchProps {
+  value: string;
+  searchTerm: string;
+  // setSearchTerm: string;
+  onChange: (value: string) => void;
+}
+
+export default function SearchBasicExample({ searchTerm, setSearchTerm }: SearchProps): JSX.Element {
+   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div className="relative mb-4 flex items-center w-80 sm:w-96 p-8">
+        <input
+          type="search"
+          className="w-full relative m-6 -mr-0.5 block w-[1px] flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="button-addon1"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        {/*SMALL SCREEN   w-80 */}
+
+        {/* <!--Search button--> */}
+        {/* <TERipple color='light'> */}
+        <button
+          className="mt-6 mb-6 relative z-[2] flex items-center rounded-r bg-primary px-6 py-2 text-xs font-medium uppercase leading-tight bg-neutral-400"
+          type="button"
+          id="button-addon1"
+          >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+            fillRule="evenodd"
+            d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+            clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        {/* </TERipple> */}
+      </div>
+    </div>
+  );
+}
